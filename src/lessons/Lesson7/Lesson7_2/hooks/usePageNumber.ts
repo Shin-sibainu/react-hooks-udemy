26445f86aed7c3fe22ca5420cef1d9956293a127
Shin-sibainu <shincode0712@gmail.com>
2024-03-17 17:39:12 +0900

    add lesson7_2

--- src/lessons/Lesson7/Lesson7_2/hooks/usePageNumber.ts
+++ src/lessons/Lesson7/Lesson7_2/hooks/usePageNumber.ts
@@ -4,21 +4,21 @@ export const usePageNumber = () => {
   const [page, setPage] = useState<number>(1);
   const [isPending, startTransition] = useTransition();
 
-  //   const incrementPage = useCallback(() => {
-  //     startTransition(() => setPage((p) => p + 1));
-  //   }, [startTransition]);
-
   const incrementPage = useCallback(() => {
-    setPage((p) => p + 1);
-  }, []);
+    startTransition(() => setPage((p) => p + 1));
+  }, [startTransition]);
 
-  //   const decrementPage = useCallback(() => {
-  //     startTransition(() => setPage((p) => Math.max(p - 1, 1)));
-  //   }, [startTransition]);
+  // const incrementPage = useCallback(() => {
+  //   setPage((p) => p + 1);
+  // }, []);
 
   const decrementPage = useCallback(() => {
-    setPage((p) => Math.max(p - 1, 1));
-  }, []);
+    startTransition(() => setPage((p) => Math.max(p - 1, 1)));
+  }, [startTransition]);
+
+  // const decrementPage = useCallback(() => {
+  //   setPage((p) => Math.max(p - 1, 1));
+  // }, []);
 
   return useMemo(
     () => ({
